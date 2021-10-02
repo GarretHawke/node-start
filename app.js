f5939bb3a287b693b7324b15f68b0462f7e89798
GarretHawke <alexandra17au@gmail.com>
2021-10-02 15:10:39 +0300

    feat: install nodemon, express and ejs packages

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const PORT = 4000;
 
 const server = http.createServer((req, res) => {
   console.log('Server request');
+  console.log('Just for test');
 
   res.setHeader('Content-Type', 'text/html');
 
