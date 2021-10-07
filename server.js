6173d49534c376d52b17366d1bfaaead83d0a58a
GarretHawke <alexandra17au@gmail.com>
2021-10-07 22:36:07 +0300

    feat: add update and delete method

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Post = require('./models/post');
 const Contact = require('./models/contact');
 
@@ -28,6 +29,8 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static('styles'));
+
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
   const title = 'Home';
@@ -60,6 +63,42 @@ app.get('/posts/:id', (req, res) => {
     });
 });
 
+app.delete('/posts/:id', (req, res) => {
+  const title = 'Post';
+  Post
+    .findByIdAndDelete(req.params.id)
+    .then((result) => {
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error', { title: 'Error' }));
+    });
+});
+
+app.get('/edit/:id', (req, res) => {
+  const title = 'Edit Post';
+  Post
+    .findById(req.params.id)
+    .then(post => res.render(createPath('edit-post'), { post, title }))
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), { title: 'Error' });
+    });
+});
+
+app.put('/edit/:id', (req, res) => {
+  const { title, author, text } = req.body;
+  const { id } = req.params;
+  Post
+    .findByIdAndUpdate(id, { title, author, text })
+    .then((result) => res.redirect(`/posts/${id}`))
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), { title: 'Error' });
+    });
+});
+
 app.get('/posts', (req, res) => {
   const title = 'Posts';
   Post
@@ -72,6 +111,11 @@ app.get('/posts', (req, res) => {
     });
 });
 
+app.get('/add-post', (req, res) => {
+  const title = 'Add post';
+  res.render(createPath('add-post'), { title });
+});
+
 app.post('/add-post', (req, res) => {
   const { title, author, text } = req.body;
   const post = new Post({ title, author, text });
@@ -82,11 +126,6 @@ app.post('/add-post', (req, res) => {
       console.log(error);
       res.render(createPath('error'), { title: 'Error' });
     });
-});
-
-app.get('/add-post', (req, res) => {
-  const title = 'Add post';
-  res.render(createPath('add-post'), { title });
 });
 
 app.use((req, res) => {
