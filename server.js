8ee340d9ec5eed0fa4b8c9a6300c40a2c5b5b4da
GarretHawke <alexandra17au@gmail.com>
2021-10-07 20:49:59 +0300

    feat: add mongoose

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Post = require('./models/post');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
 const PORT = 3000;
+const db = 'mongodb+srv://aleksandra:4815162342Aug@cluster0.z9sqb.mongodb.net/node-blog?retryWrites=true&w=majority';
+
+mongoose
+  .connect(db)
+  .then((res) => console.log('Connected to database'))
+  .catch((error) => console.log(error));
 
 const createPath = (page) => path.resolve(__dirname, 'ejs-views', `${page}.ejs`);
 
@@ -67,14 +75,14 @@ app.get('/posts', (req, res) => {
 
 app.post('/add-post', (req, res) => {
   const { title, author, text } = req.body;
-  const post = {
-    id: new Date(),
-    date: (new Date()).toLocaleDateString(),
-    title,
-    author,
-    text,
-  }
-  res.render(createPath('post'), { post, title });
+  const post = new Post({ title, author, text });
+  post
+    .save()
+    .then((result) => res.send(result))
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), { title: 'Error' });
+    });
 });
 
 app.get('/add-post', (req, res) => {
